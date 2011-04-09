1f90353d38845924c39328c5d119ad4aaa2ccdd9
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-08 21:42:03 -0700

    Install jQuery and jQuery-UJS

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,11 @@ module Chatterbox
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    config.action_view.javascript_expansions[:defaults] = %w(
+      jquery-1.5.2.min
+      jquery-ui-1.8.11.min
+      rails
+    )
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
