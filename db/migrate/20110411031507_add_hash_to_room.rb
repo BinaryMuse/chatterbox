fc7f998715101621c9e16edaa960224ea84749d9
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-10 21:33:59 -0700

    Fix migration!

--- db/migrate/20110411031507_add_hash_to_room.rb
+++ db/migrate/20110411031507_add_hash_to_room.rb
@@ -1,10 +1,6 @@
 class AddHashToRoom < ActiveRecord::Migration
-  class Room < ActiveRecord::Base; end
-
   def self.up
     add_column :rooms, :hash, :string
-    Room.reset_column_information
-    Room.all.each { |r| r.hash = Digest::SHA1.hexdigest(r.name); r.save! }
   end
 
   def self.down
