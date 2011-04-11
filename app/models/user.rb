7ee9359c531e4837099809297d865d9321ff44c3
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-11 13:29:43 -0700

    Users do not need password to edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :username, :password, :password_confirmation, :remember_me
 
+  def password_required?
+    new_record?
+  end
+
   def self.find_for_facebook_oauth(access_token, signed_in_resource = nil)
     data = access_token['extra']['user_hash']
     if user = User.find_by_email(data["email"])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Chatterbox::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :registrations => "registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :rooms do
     get  :join,   :on => :collection
     post :search, :on => :collection
