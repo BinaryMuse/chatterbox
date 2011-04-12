66baab72213060089edbe16b13d0fd1b8e2bf848
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-12 08:22:48 -0700

    Fix notifications

--- public/javascripts/chatterbox.js
+++ public/javascripts/chatterbox.js
@@ -1,7 +1,9 @@
 Chatterbox = {
   focused: true,
-  notified: false,
+  notifying: false,
+  alt_title: false,
   interval: '',
+  notification: '',
 
   user_joined: function(user) {
     Chatterbox.append_text("<b>" + user + " has joined the room.</b>");
@@ -15,38 +17,59 @@ Chatterbox = {
     Chatterbox.append_text("<b>" + user + "</b>: " + message);
   },
 
-  set_focused: function(focused) {
-    Chatterbox.focused = focused;
+  set_focused: function(focus) {
+    console.log("Focused: " + focus);
+    Chatterbox.focused = focus;
     if(Chatterbox.focused) {
       Chatterbox.stop_notifying();
     }
   },
 
+  notify: function() {
+    if(!Chatterbox.focused && !Chatterbox.notifying) {
+      console.log("notify(): starting the notifications");
+      Chatterbox.start_notifying();
+    } else {
+      console.log("notify(): incrementing the notifications");
+      Chatterbox.notification++;
+    }
+  },
+
   start_notifying: function() {
-    Chatterbox.interval = setInterval(Chatterbox.flash, 1000);
+    Chatterbox.notifying = true;
+    Chatterbox.notification = 1;
+    Chatterbox.interval = window.setTimeout(Chatterbox.flash, 1000);
   },
 
   stop_notifying: function() {
-    clearInterval(Chatterbox.interval);
+    console.log("stop_notifying(): stopping the notifications");
+    window.clearInterval(Chatterbox.interval);
     document.title = "Chatterbox";
+    Chatterbox.notification = "";
+    Chatterbox.notifying = false;
   },
 
   flash: function() {
-    if(Chatterbox.notified) {
+    if(Chatterbox.alt_title) {
       document.title = "Chatterbox";
     } else {
-      document.title = "(*) Chatterbox";
+      document.title = "(" + Chatterbox.notification + ") Chatterbox";
     }
-    Chatterbox.notified = !Chatterbox.notified;
+    Chatterbox.alt_title = !Chatterbox.alt_title;
+
+    if(!Chatterbox.focused) {
+      console.log("flash(): not focused, resetting timeout");
+      window.setTimeout(Chatterbox.flash, 1000);
+    } else {
+      document.title = "Chatterbox";
+    }
   },
 
   append_text: function(text) {
     el = $("<p>").html(text);
     $("#chatlog").append(el);
     Chatterbox.scroll_down();
-    if(!Chatterbox.focused && !Chatterbox.notifying) {
-      Chatterbox.start_notifying();
-    }
+    Chatterbox.notify();
   },
 
   scroll_down: function() {
